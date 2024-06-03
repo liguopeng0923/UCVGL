95c0dd2cfb2386fa53fbc514af1bac68ad193bad
Guopeng Li <61257834+liguopeng0923@users.noreply.github.com>
2024-06-03 12:37:13 +0800

    Update run_CVUSA.sh

--- run_CVUSA.sh
+++ run_CVUSA.sh
@@ -11,15 +11,15 @@ python test.py --dataroot ../../data/CVUSA/ --name CVUSA --model cycle_gan --res
 cd ../../
 
 # 3. Intra-View Self-Supervised Learning
-python -u train_intraview.py --lr 0.0001 --dist-url 'tcp://localhost:10000' --multiprocessing-distributed --world-size 1 --rank 0  --epochs 8 --op adamw --dataset cvusa --data-folder "./data/CVUSA" --batch-size 128 --workers=16 
+python -u train_intraview.py --lr 0.0001 --dist-url 'tcp://localhost:10000' --multiprocessing-distributed --world-size 1 --rank 0  --epochs 12 --op adamw --dataset cvusa --data-folder "./data/CVUSA" --batch-size 128 --workers=16 
 
 # 4. Cross-View Self-Supervised Learning
-python -u train_crossview_fake.py --lr 0.0001 --dist-url 'tcp://localhost:10000' --multiprocessing-distributed --world-size 1 --rank 0  --epochs 16 --op adamw --dataset cvusa --data-folder "./data/CVUSA" --batch-size 128 --workers=16 --checkpoint ./ckpt/cvusa/train_intraview.pth.tar
+python -u train_crossview_fake.py --lr 0.0001 --dist-url 'tcp://localhost:10000' --multiprocessing-distributed --world-size 1 --rank 0  --epochs 28 --op adamw --dataset cvusa --data-folder "./data/CVUSA" --batch-size 128 --workers=16 --checkpoint ./ckpt/cvusa/train_intraview.pth.tar
 
 # 5. Semi-Supervised Curriculum Learning
-python -u train_crossview_sat.py --lr 0.0001 --dist-url 'tcp://localhost:10000' --multiprocessing-distributed --world-size 1 --rank 0  --epochs 32 --op adamw --dataset cvusa --data-folder "./data/CVUSA" --batch-size 128 --workers=16 --checkpoint ./ckpt/cvusa/train_crossviewfake.pth.tar --threshold 0.05
+python -u train_crossview_sat.py --lr 0.0001 --dist-url 'tcp://localhost:10000' --multiprocessing-distributed --world-size 1 --rank 0  --epochs 40 --op adamw --dataset cvusa --data-folder "./data/CVUSA" --batch-size 128 --workers=16 --checkpoint ./ckpt/cvusa/train_crossviewfake.pth.tar --threshold 0.05
 
 # Evaluation
-#python -u train_crossview_sat.py --lr 0.0001 --dist-url 'tcp://localhost:10000' --multiprocessing-distributed --world-size 1 --rank 0  --epochs 32 --op adamw --dataset cvusa --data-folder "./data/CVUSA" --batch-size 128 --workers=16 --checkpoint ./ckpt/train_crossviewsat.pth.tar --threshold 0.05 --evaluate
+#python -u train_crossview_sat.py --lr 0.0001 --dist-url 'tcp://localhost:10000' --multiprocessing-distributed --world-size 1 --rank 0  --epochs 40 --op adamw --dataset cvusa --data-folder "./data/CVUSA" --batch-size 128 --workers=16 --checkpoint ./ckpt/train_crossviewsat.pth.tar --threshold 0.05 --evaluate
 
 
